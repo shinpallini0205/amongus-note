aae13a25896a8d4ab7d406a0b107f5f29a5209af
shinpallini0205 <parinpallini@gmail.com>
2021-11-30 02:35:43 +0000

    feat: add undo button to delete a wrongly added. fix: change comments to "#" to avoid streamlit's Magic.

--- python/work/app.py
+++ python/work/app.py
@@ -8,8 +8,8 @@ if 'alive' not in st.session_state:
 
 st.title('Among us Note')
 
-"""Create player list from text_area.
-"""
+# Create player list from text_area.
+
 st.header('Player list')
 players = st.text_area("Please write player name.")
 player_list = players.split()
@@ -17,32 +17,36 @@ st.write('Player list')
 st.write(player_list)
 
 
-"""Clear dead player list
-"""
+# Clear dead player list
+
 clear_flag = st.button("Clear")
 if clear_flag:
     st.session_state['dead'] = []
     st.session_state['alive'] = []
 
 
-"""Create dead player list
-"""
+# Create dead player list
+
 
 killed = st.selectbox("Select killed player...", options=player_list)
 append_flag = st.button("Click me")
 if append_flag:
     st.session_state['dead'].append(killed)
 
+undo_flag = st.button("Undo dead player")
+if undo_flag:
+    st.session_state['dead'].pop(-1)
+
 st.write(st.session_state['dead'])
 
-"""Create alive player list
-"""
+# Create alive player list
+
 
 st.session_state['alive'] = [player for player in player_list if not player in st.session_state['dead']]
 st.write(st.session_state['alive'])
 
-"""Expander test
-"""
+# Expander test
+
 
 for killed_player in st.session_state['dead']:
     with st.expander(killed_player):
