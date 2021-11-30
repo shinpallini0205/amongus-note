97925ce7db6cb301ad2a83985440f4918daa7f8d
shinpallini0205 <parinpallini@gmail.com>
2021-11-30 03:04:55 +0000

    feat: set placeholder for help to write name. fix: rename button labels to simplicity.

--- python/work/app.py
+++ python/work/app.py
@@ -11,11 +11,13 @@ st.title('Among us Note')
 # Create player list from text_area.
 
 st.header('Player list')
-players = st.text_area("Please write player name.")
+players = st.text_area(
+    "Please write player name.",
+     placeholder="Player_name1\nPlayer_name2\nPlayer_name3\n..."
+)
 player_list = players.split()
 st.write('Player list')
 st.write(player_list)
-
 
 # Clear dead player list
 
@@ -27,13 +29,12 @@ if clear_flag:
 
 # Create dead player list
 
-
 killed = st.selectbox("Select killed player...", options=player_list)
-append_flag = st.button("Click me")
+append_flag = st.button("Add killed player")
 if append_flag:
     st.session_state['dead'].append(killed)
 
-undo_flag = st.button("Undo dead player")
+undo_flag = st.button("Undo killed player")
 if undo_flag:
     st.session_state['dead'].pop(-1)
 
@@ -41,12 +42,10 @@ st.write(st.session_state['dead'])
 
 # Create alive player list
 
-
 st.session_state['alive'] = [player for player in player_list if not player in st.session_state['dead']]
 st.write(st.session_state['alive'])
 
 # Expander test
-
 
 for killed_player in st.session_state['dead']:
     with st.expander(killed_player):
