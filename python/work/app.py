67d36de5f2c5ec7339ad220126466e3bdfadc9d4
shinpallini0205 <parinpallini@gmail.com>
2021-12-01 01:36:19 +0000

    feat: キル以外で生存者じゃなくなったものをカウントするために追放者リストを作成。 feat: 追放者の追加やundoを実装 feat: 生存者の数を正しくカウントするためにキルプレイヤーと追放者の集合の合計から生存者を求める仕組みに変更

--- python/work/app.py
+++ python/work/app.py
@@ -28,24 +28,36 @@ clear_flag = st.button("キルされたプレイヤー情報をリセット")
 if clear_flag:
     st.session_state['dead'] = []
     st.session_state['alive'] = []
-
+    st.session_state['ejected'] = []
 
 # Create dead player list
 
-killed = st.selectbox("今回キルされたのは...", options=player_set)
-append_flag = st.button("送信")
-if append_flag:
-    st.session_state['dead'].append(killed)
+selected_player = st.selectbox("プレイヤーを１人選んでください", options=player_set)
+killed_flag = st.button("キル")
+if killed_flag:
+    st.session_state['dead'].append(selected_player)
 
-undo_flag = st.button("１人戻す")
+undo_flag = st.button("キルから1人戻す")
 if undo_flag:
     st.session_state['dead'].pop(-1)
 
 st.write(st.session_state['dead'])
 
+# Create ejected player list
+
+ejected_flag = st.button("追放")
+if ejected_flag:
+    st.session_state['ejected'].append(selected_player)
+
+undo_ejected_flag = st.button("追放から1人戻す")
+if undo_ejected_flag:
+    st.session_state['ejected'].pop(-1)
+
+st.write(st.session_state['ejected'])
+
 # Create alive player list
 
-st.session_state['alive'] = [player for player in player_set if not player in st.session_state['dead']]
+st.session_state['alive'] = [player for player in player_set if not player in (st.session_state['dead'] + st.session_state['ejected'])]
 st.write(st.session_state['alive'])
 
 # Expander test
